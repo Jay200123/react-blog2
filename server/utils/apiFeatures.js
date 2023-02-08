80e9a99467737d05c8b79589fe7645c936c039a0
Jay200123 <kenken.ayonon@gmail.com>
2023-02-08 09:52:20 +0800

    commit

--- server/utils/apiFeatures.js
+++ server/utils/apiFeatures.js
@@ -40,59 +40,45 @@ class APIFeatures {
 
     }
 
-    // products?keyword=apple&price[gte]=1&price[lte]=200
+    filter() {
 
-   filter() {
-   
-   
-   
-           const queryCopy = { ...this.queryStr };
-   
-           // console.log(queryCopy);
-   
-           // Removing fields from the query
-   
-           const removeFields = ['keyword', 'limit', 'page']
-   
-           removeFields.forEach(el => delete queryCopy[el]);
-   
-           
-   
-           // Advance filter for price, ratings etc
-   
-           let queryStr = JSON.stringify(queryCopy);
-   
-           console.log(queryStr);
-   
-           queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`)
-   
-           console.log(queryStr);
-   
-           this.query = this.query.find(JSON.parse(queryStr));
-   
-           console.log(JSON.parse(queryStr));
-   
-           return this;
-   
-       }
-   
-   
-   
-    pagination(resPerPage) {
-   
-           const currentPage = Number(this.queryStr.page) || 1;
-   
-           const skip = resPerPage * (currentPage - 1);
-   
-   
-   
-           this.query = this.query.limit(resPerPage).skip(skip);
-   
-           return this;
-   
-       }
+        const queryCopy = { ...this.queryStr};
+        console.log(queryCopy)
 
+        const removeFields = ['keyword', 'limit', 'page']
+
+        removeFields.forEach(el => delete queryCopy[el]);
+
+        let queryStr = JSON.stringify(queryCopy);
+
+        console.log(queryStr);
+
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`)
+
+        console.log(queryStr);
+
+        this.query = this.query.find(JSON.parse(queryStr));
+
+        console.log(JSON.parse(queryStr));
+
+        return this;
+        
+        }
+
+
+        pagination(resPerPage) {
+
+            const currentPage = Number(this.queryStr.page) || 1;
     
+            const skip = resPerPage * (currentPage - 1);
+    
+    
+    
+            this.query = this.query.limit(resPerPage).skip(skip);
+    
+            return this;
+    
+        }
 
    }
 
